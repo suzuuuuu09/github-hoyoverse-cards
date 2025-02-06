baf35755b45df50c9cd5306c2af2a9555ed33747
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2025-02-06 16:48:13 +0900

    🚀 テストデプロイ

--- api/main.py
+++ api/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
 
 app = FastAPI()
 
 @app.get("/")
 def index():
     return {"message": "Hello world!"}
+
+@app.get("/image")
+async def get_image():
+    return FileResponse("api/assets/img/clip.png", media_type="image/png")
 
 if __name__ == "__main__":
     import uvicorn
